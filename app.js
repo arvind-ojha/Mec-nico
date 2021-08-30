968354df7afacd70fe86b44d25413bb9bc5c9a94
ks15raj <62212221+ks15raj@users.noreply.github.com>
2021-08-30 09:07:26 +0530

    Signup/login

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const ejs = require('ejs')
+
+const app = express();
+app.set('view engine','ejs')
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+app.get('/signup',(req,res)=>{
+    res.render('signup')
+})
+app.listen(3000, () => {
+    console.log(`Server started on port`);
+});
