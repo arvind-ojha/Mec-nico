fec3930384015612b22ca6906d13ba044bbf1946
imrcht <rachchooit@gamil.com>
2021-08-30 20:10:35 +0530

    Added retailer page

--- app.js
+++ app.js
@@ -2,17 +2,30 @@ const express = require('express');
 const ejs = require('ejs')
 
 const app = express();
+
+
 app.set('view engine','ejs')
 app.use(express.static('public'));
+
+
 app.get('/',(req,res)=>{
     res.render('index')
 })
+
 app.get('/login',(req,res)=>{
     res.render('login')
 })
+
+
 app.get('/signup',(req,res)=>{
-    res.render('signup')
+    res.render('signup', {
+        type: req.query.type,
+    })
 })
+
+
+
+
 app.listen(3000, () => {
     console.log(`Server started on port 3000`);
 });
