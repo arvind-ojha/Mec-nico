3fd57b2521dce91b35adf6ab78a1b3f4944a4213
arvind-ojha <arvindozha359@gmail.com>
2021-09-07 18:04:09 +0530

    user api

---
+++ app.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const express = require('express');
+require('dotenv').config();
+const morgan = require('morgan');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
+
+// Load env vars
+
+
+// Connect to database
+connectDB();
+
+// Route files
+const user = require('./routes/user');
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+// app.use(cookieParser());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount routers
+// app.use('/api/v1/bootcamps', bootcamps);
+// app.use('/api/v1/courses', courses);
+// app.use('/api/v1/auth', auth);
+app.use('/api/v1/user', user);
+// app.use('/api/v1/reviews', reviews);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    )
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    // server.close(() => process.exit(1));
+});
